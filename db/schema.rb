f2746a6fdf223651c4f7394377b5d9f83fe7d074
alan vergara <alan_vergara_p@hotmail.com>
2022-09-28 13:46:38 -0500

    install device

---
+++ db/schema.rb
@@ -0,0 +1,80 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_09_26_214837) do
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "company_vehicles", force: :cascade do |t|
+    t.string "brand"
+    t.integer "model"
+    t.string "license_plate"
+    t.integer "company_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_company_vehicles_on_company_id"
+  end
+
+  create_table "entrances", force: :cascade do |t|
+    t.date "date"
+    t.time "time"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "destination"
+    t.text "comments"
+    t.integer "mileage"
+    t.integer "user_id", null: false
+    t.integer "company_vehicle_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_vehicle_id"], name: "index_entrances_on_company_vehicle_id"
+    t.index ["user_id"], name: "index_entrances_on_user_id"
+  end
+
+  create_table "exits", force: :cascade do |t|
+    t.date "date"
+    t.time "time"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "destination"
+    t.text "comments"
+    t.integer "mileage"
+    t.string "invoice"
+    t.integer "user_id", null: false
+    t.integer "company_vehicle_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_vehicle_id"], name: "index_exits_on_company_vehicle_id"
+    t.index ["user_id"], name: "index_exits_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "company_vehicles", "companies"
+  add_foreign_key "entrances", "company_vehicles"
+  add_foreign_key "entrances", "users"
+  add_foreign_key "exits", "company_vehicles"
+  add_foreign_key "exits", "users"
+end
