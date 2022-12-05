ed075664e2a8b1c3e90695c6f95c7c79a9452db0
alan vergara <alan_vergara_p@hotmail.com>
2022-12-05 16:23:15 -0600

    dynamic dropdowns in entrances/view and exits/views now hides input fields when company is = particular

--- app/javascript/controllers/company_controller.js
+++ app/javascript/controllers/company_controller.js
@@ -4,12 +4,27 @@ import { get } from "@rails/request.js"
 export default class extends Controller {
   // static targets = [vehicleSelect] esta era la opcion que proponía el video, hubo que buscar un plan "B"
   change(event) {
+    let companyName = event.target.selectedOptions[0].text; // esto me devuelve el Texto HTML del input.
+    let target = this.targets.find("vehicleSelect").id; // Este fue el plan "B", y se le pide el ID para que se lo podamos pasar en la URL de la linea 11.
+    let entrancesAndExitsMileage = document.getElementsByClassName("mileage_input");
+
     let companyId = event.target.value;
-    let target = this.targets.find("vehicleSelect").id // Este fue el plan "B", y se le pide el ID para que se lo podamos pasar en la URL de la linea 11.
-    // let target = "entrance_company_vehicle_id"
-    console.log(target)
     get(`/companies/vehicles?target=${target}&company=${companyId}`, {
       responseKind: "turbo-stream"
     })
+
+    if (companyName === 'Particular'){
+      entrancesAndExitsMileage[0].style.display = "none";
+      entrancesAndExitsMileage[0].getElementsByTagName("input")[0].value = 0;
+
+      let invoiceInput = document.getElementById('exit_invoice');
+      if (invoiceInput != null) {
+        invoiceInput.style.display = "none";
+        invoiceInput.value = "No aplica";
+      };
+
+    } else {
+      entrancesAndExitsMileage[0].style.display = "block";
+    }
   }
 }
